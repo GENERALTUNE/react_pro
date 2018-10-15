bf74d6a9161c74a08b7d20ff383661bc44296f08
nclight <nclight@126.com>
2018-10-15 14:57:03 +0800

    修改package配置

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function component() {
-//   var element = document.createElement('div');
 
-//   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Welcome  name="jack cheng" />,
   document.getElementById('root')
 );
+
+
+// function component() {
+//   var element = document.createElement('div');
+//   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   return element;
+// }
+// document.body.appendChild(component());
