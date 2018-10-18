6ff8b8854f05de93afd885abb49f8449dddb0d00
nclight <nclight@126.com>
2018-10-18 19:13:11 +0800

    解决webpack bable 升级问题

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 
 class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+	constructor(props) {
+	    super(props);
+	    this.state = {name: ''};
+	}
+
+
+	handleAlert = () => {
+	    console.log('#####################');
+	}
+
+	handleInput = (evt) => {
+	  	// console.log('当前值是：', evt.target.value)
+	  	this.setState({name: evt.target.value});
+	}
+
+  	render() {
+    	return (<><h1 onClick={this.handleAlert} >Hello, {(this.state.name || this.props.name)}</h1>
+    			请输入：<input onChange={this.handleInput} /></>);
+  	}
 }
 
 ReactDOM.render(
   <Welcome  name="jack cheng" />,
   document.getElementById('root')
 );
-
-
-// function component() {
-//   var element = document.createElement('div');
-//   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   return element;
-// }
-// document.body.appendChild(component());
